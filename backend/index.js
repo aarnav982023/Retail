0bfa7324d9aa795ca53c256e089ecd62e220e380
Pushp Vashisht <pushptyagi1@gmail.com>
2020-01-11 19:38:46 +0530

    User model and User registration

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./routes/routing');
+const connectDB = require('./config/db');
+
+app = express();
+
+//Connecting to the database
+connectDB();
+
+app.use(bodyParser.json());
+app.use('/', router);
+
+//process.env.PORT will pickup the port from environment variable when deployed on Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
