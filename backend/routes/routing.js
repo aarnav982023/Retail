89ce7f2d57c72e9a5ad154a894bffc15cd708a3b
Pushp Vashisht <pushptyagi1@gmail.com>
2020-01-20 00:42:05 +0530

    Get Menu Item List API

--- backend/routes/routing.js
+++ backend/routes/routing.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('./api/users');
+const menuListItem = require('./api/menuListItem');
 const routing = express.Router();
 
 // Define Routes
 routing.get('/', (req, res) => res.send('API Running'));
 routing.use('/api/users', users);
+routing.use('/api/menuListItem', menuListItem);
 
 module.exports = routing;
